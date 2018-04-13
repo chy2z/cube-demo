e8680ffd6f6acf89db8f818d09212822e5b8bb6a
chenghongyan <chenghy.js@eakay.cn>
2018-04-13 16:36:04 +0800

    代码提交

--- src/view/My.vue
+++ src/view/My.vue
@@ -1,42 +1,26 @@
 <template>
-	<div class="cubes-layout">
-		
+	<div class="cubes-layout">		
     <header class="cubes-head">
       <h1>我的</h1>      
       <i class="cubes-head-kefu iconfont">&#xe625;</i>
       <i class="cubes-head-code iconfont">&#xe632;</i>
-    </header>
-    
+    </header>    
     <section class="cubes-content">
-     
-    </section>  
-    
+           <div class="cubes-content-top">
+             
+           </div>
+           <div class="cubes-content-menu">
+           
+           </div>
+    </section>
     </div>
 </template>
 
 <script type="text/ecmascript-6">  
-
   export default {
     data() {
       return {
-        components: [
-          {
-            path: '/Login',
-            text: '登录'
-          },
-          {
-            path: '/Setting',
-            text: '设置'
-          },
-          {
-            path: 'Register',
-            text: '注册'
-          },
-          {
-            path: '/radio',
-            text: 'Radio'
-          }          
-        ]
+        
       }
     },
     components: {
@@ -46,21 +30,28 @@
 </script>
 
 <style scoped="scoped" lang="less">
-
-.cubes-head .cubes-head-kefu{
+.cubes-head {
+	.cubes-head-kefu{
         position: absolute;
         top: 0;
         right: 0;
         padding: 0 15px;        
         cursor: pointer;
-}
-
-.cubes-head .cubes-head-code{
+    }
+    .cubes-head-code{
         position: absolute;
         top: 0;
         right: 0;
         padding: 0 60px;   
         cursor: pointer;     
+    }
+}
+
+.cubes-content  {
+	.cubes-content-top{
+		 height: 100px;		 
+		 background-image: url(../assets/my-bg.jpg);
+	}
 }
 
 </style>
